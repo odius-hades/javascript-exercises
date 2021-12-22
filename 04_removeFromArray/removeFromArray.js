fedd293d3f5c1a2360d8784314c42e418d1b05b8
odius-hades <flgraveson@gmail.com>
2021-12-22 15:31:50 -0500

    Pass 6/7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
-
+function removeFromArray(arr,num,...moreNum){
+    if(moreNum.length > 0){
+        let arrDiff = arr.filter(x => !moreNum.includes(x));
+        return arrDiff.filter(function(remove){
+            return remove != num;
+        });
+    } else {
+        return arr.filter(function(remove){
+            return remove != num;
+        });
+    }
+}
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//if arr.typeOf !== num.typeOf || arr.typeOf !== moreNum.typeOf
